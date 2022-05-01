62dc60ad483a87ffa70ca129e04d942966783dcf
Zeustakeshi <95726917+Zeustakeshi@users.noreply.github.com>
2022-05-01 18:59:18 +0700

    Update handleAsRange.js

--- js/action/handleAsRange.js
+++ js/action/handleAsRange.js
@@ -1,50 +1,41 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
-const handleAsRange =  (correctAnswer ,rounds , highScore , isHistory) => {
+const handleAsRange =  (correctAnswer ,rounds , highScore , index) => {
     const percentageRounds = highScore.rounds !== 0 ? 
         Math.floor(rounds / highScore.rounds * 100) : 100
     const handleAddModifier = (varNeedToCompare,selector) =>{
-        if(!selector[0] ) {
+        if(!selector[index] ) {
             return false
         }
         if (varNeedToCompare <= 45) {
-            selector[0].classList.add('as-range--low')
+            selector[index].classList.add('as-range--low')
         }else if(varNeedToCompare > 45 && varNeedToCompare <= 70){
-            selector[0].classList.add('as-range--medium')
+            selector[index].classList.add('as-range--medium')
            
         }else{
-            selector[0].classList.add('as-range--high')
+            selector[index].classList.add('as-range--high')
         }
         const asRangeSpan = $$('.as-range span')
         const asRangeOldSpan = $$('.as-range-old span')
         const asRangeRoundsSpan = $$('.as-range-rounds span')
-
-
-        asRangeSpan[0] 
+        asRangeSpan[index] 
         ? 
-            asRangeSpan[0].style.width = `${correctAnswer}%` 
+            asRangeSpan[index].style.width = `${correctAnswer}%` 
         : false
 
-        asRangeOldSpan[0] ?
-            asRangeOldSpan[0].style.width = `${highScore.correctAnswer}%` 
+        asRangeOldSpan[index] ?
+            asRangeOldSpan[index].style.width = `${highScore.correctAnswer}%` 
         : false
 
-        asRangeRoundsSpan[0] ?
-            asRangeRoundsSpan[0].style.width = `${percentageRounds}%` 
+        asRangeRoundsSpan[index] ?
+            asRangeRoundsSpan[index].style.width = `${percentageRounds}%` 
         : false
     }
-    if (isHistory){
+    setTimeout(() => {
         handleAddModifier(highScore.correctAnswer , $$('.as-range-old'))
         handleAddModifier(percentageRounds,$$('.as-range-rounds'))
-        handleAddModifier(correctAnswer,$$('.as-range'))   
-    }
-    else{
-        setTimeout(() => {
-            handleAddModifier(highScore.correctAnswer , $$('.as-range-old'))
-            handleAddModifier(percentageRounds,$$('.as-range-rounds'))
-            handleAddModifier(correctAnswer,$$('.as-range'))            
-        },500);
-    }
+        handleAddModifier(correctAnswer,$$('.as-range'))            
+    },500);
 }
 
 export default handleAsRange
